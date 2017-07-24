72f3c4d13a4ee9b0ca996145b996afac0a920fc2
Jake Backer <jbacker42@gmail.com>
2017-07-24 17:32:08 -0400

    Begin testing testing

---
+++ test/org/robockets/robot/RobotTest.java
@@ -0,0 +1,14 @@
+package org.robockets.robot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RobotTest {
+
+	@Test
+	public void test() {
+		assertTrue("Nothing to test right now", true);
+	}
+
+}
